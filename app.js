75bdd8f6028ebcc6153d5ab59db54ba6809feb77
Yashi300 <guptayashi7103@gmail.com>
2022-09-07 16:48:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express=require("express");
+const path=require("path");
+const app=express();
+const port=800;
+
+
+app.use('/static',express.static('static'))
+app.use(express.urlencoded())
+
+app.set('view engine','pug')
+app.set('views',path.join(__dirname,'views'))
+
+app.get('/',(req,res)=>{
+    const param={}
+    res.status(200).render('home.pug',param);
+})
+
+app.get('/contact',(req,res)=>{
+    const param={}
+    res.status(200).render('contact.pug',param);
+})
+app.get('/about',(req,res)=>{
+    const param={}
+    res.status(200).render('about.pug',param);
+})
+
+
+app.listen(port,(req,res)=>{
+    console.log(`The app started successfully on port ${port}`);
+})
